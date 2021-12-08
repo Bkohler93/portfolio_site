1174bc1a144336376a62014c942915080b339a51
Brett Kohler <brettkohler93@gmail.com>
2021-12-07 17:20:33 -0800

    initial commit. added up to side drawer

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Container from "@mui/material/Container";
+
+const Homepage = () => {
+  return (
+    <Container maxWidth="sm">
+    <h1>HomePage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eveniet repudiandae autem molestiae facere ipsa voluptas, natus itaque dolorem aliquid cumque libero optio similique laboriosam dolor labore nobis nisi nemo.</p>
+    </Container>
+  );
+};
+
+export default Homepage;
